053e9a0023bc3cd5a5e75a2b9a394dba1429209b
Samuel Birhanu <samuel.ryzen12@gmail.com>
2023-11-21 20:19:26 +0300

    new

--- FILELOCK.py
+++ FILELOCK.py
@@ -1,6 +1,6 @@
 # (C) 2023 BIGG SMOKE
 
-import os, getpass
+import os, getpass, sys
 from Crypto import Random
 from Crypto.Cipher import AES
 from Crypto.Protocol.KDF import PBKDF2
@@ -70,16 +70,19 @@ def main():
     print(" " * 16 + "[1] Encrypt")
     print(" " * 16 + "[2] Decrypt\n")
     while True:
-        choice = int(input(" " * 3 + "Choose an option: "))
-        match choice:
-            case 1:
-                enc()
-                break
-            case 2:
-                denc()
-                break
-            case _:
-                print("Invalid Option!")
+        try:
+            choice = int(input(" " * 3 + "Choose an option: "))
+            match choice:
+                case 1:
+                    enc()
+                    break
+                case 2:
+                    denc()
+                    break
+                case _:
+                    print("Invalid Option!")
+        except ValueError:
+            print("Invalid option!")
 
 
 def enc():
@@ -120,5 +123,38 @@ def denc():
         exit(1)
 
 
+def help():
+    print(
+    "FILELOCK\n\n(C) 2023 THE LUNAR SURFACE\n\nDISCLAIMER: We (THE LUNAR SURFACE) will be not held responsible for bricked phones, lost important files, ransomware attacks and even the death of your cat (even though it doesn't have any correlation to the program but just for the hell of it). If you, in any way do not know how this program works, EXIT IMMEDIATELY.\n\n\n\nFILELOCK lets you encrypt and decrypt files using AES256 encryption. And it also happens to be an information corrupting program by accident. Use with caution.\n\n\nUSAGE:\n\nFILELOCK -l nuclear_launch_codes.bin password\n\nNOTE: you need to make sure that the file you want to encrypt and this script is in the same folder. Just for convience sake.\n\nARGUMENTS:\n-l   Locks the file\n-u   Unlocks the file\"\n-h   Displays this help message\n-i   Runs FILELOCK in interactive mode\n\nWARNING: Please do not decrypt a file which is not encrypted in the first place. It will corrupt the entire file!!!\nAnd keep in mind that during decryption, if you don't input the same password you used during encryption, it will output an entirely different file. So don't touch the file locked by FILELOCK if you don't know the right password. it will make it unreadable even if you supply the right password afterwards.\n\n"
+)
+
+
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2:
+        help()
+        print('\n\nPlease specify an argument.')
+        os.system("pause")
+    elif sys.argv[1] == "-i":
+        main()
+    elif sys.argv[1] == "-h":
+        help()
+        os.system("pause")
+
+    elif sys.argv[1] == '-l':
+        f = fileobj(sys.argv[2] , sys.argv[3])
+        f.lock()
+        print("Done locking...")
+        os.system("pause")
+
+    elif sys.argv[1] == '-u':
+        f = fileobj(sys.argv[2] , sys.argv[3])
+        f.unlock()
+        print("Done unlocking...")
+        os.system("pause")
+
+    else:
+        help()
+        print('\n\nPlease specify an argument.')
+        os.system("pause")
+
+       
